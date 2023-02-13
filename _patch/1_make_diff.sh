b8f3d1fb57d45da556188064979c66e62388f20f
NoUnique <kofmap@gmail.com>
2023-02-13 19:55:36 +0900

    Modify diff logic to apply modifications after fork of mecab-ko

--- _patch/1_make_diff.sh
+++ _patch/1_make_diff.sh
@@ -3,7 +3,11 @@
 DIR_SOURCE=mecab/mecab
 DIR_TARGET=mecab-ko/mecab-ko
 DIR_EXCEPT='*.html'
-PATH_OUTPUT=./diff.raw
+SOURCE_BASE_COMMIT_ID=415f47d6119c4387a83e9f942e4ea2d47acf5bdc
+SOURCE_LAST_COMMIT_ID=master
+TARGET_BASE_COMMIT_ID=5a169db0e00ce973cada1353d93b76bc8655b346
+TARGET_LAST_COMMIT_ID=master
+
 
 function fn_init() {
     # set working directory
@@ -11,19 +15,52 @@ function fn_init() {
     cd ${SCRIPT_DIR}
 }
 
-function fn_diff() {
+function fn_checkout() {
+    local DIR=$1
+    local COMMIT_ID=$2
+
+    cd ${DIR}
+
+    git fetch
+    git pull
+    git checkout ${COMMIT_ID}
+    git submodule update --init --recursive
+
+    cd -
+}
+
+function fn_self_diff() {
+    local DIR=$1
+    local BASE_COMMIT_ID=$2
+    local LAST_COMMIT_ID=$3
+    local OUTPUT=$4
+
+    PREVIOUS_DIR=$(pwd)
+    cd ${DIR}
+
+    git diff -wu --relative ${BASE_COMMIT_ID} ${LAST_COMMIT_ID} . > ${PREVIOUS_DIR}/${OUTPUT}
+
+    cd -
+}
+
+function fn_cross_diff() {
     local SOURCE=$1
     local TARGET=$2
     local EXCEPT=$3
     local OUTPUT=$4
 
-    diff -x ${EXCEPT} -wur --color ${SOURCE} ${TARGET} > ${OUTPUT}
+    diff -x ${EXCEPT} -wur ${SOURCE} ${TARGET} > ${OUTPUT}
 }
 
 function fn_main() {
     set -o xtrace
     fn_init
-    fn_diff ${DIR_SOURCE} ${DIR_TARGET} ${DIR_EXCEPT} ${PATH_OUTPUT}
+    fn_checkout ${DIR_SOURCE} ${SOURCE_LAST_COMMIT_ID}
+    fn_checkout ${DIR_TARGET} ${TARGET_LAST_COMMIT_ID}
+    fn_self_diff ${DIR_SOURCE} ${SOURCE_BASE_COMMIT_ID} ${SOURCE_LAST_COMMIT_ID} ./diff.source.patch
+    fn_self_diff ${DIR_TARGET} ${TARGET_BASE_COMMIT_ID} ${TARGET_LAST_COMMIT_ID} ./diff.target.patch
+    fn_checkout ${DIR_SOURCE} ${SOURCE_BASE_COMMIT_ID}
+    fn_cross_diff ${DIR_SOURCE} ${DIR_TARGET} ${DIR_EXCEPT} ./diff.cross.patch
     set +o xtrace
 }
 
--- _patch/2_apply_patch.sh
+++ _patch/2_apply_patch.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+DIR_ROOT=../
 DIR_SOURCE=mecab
-DIR_TARGET=../
-PATCH=./diff.patch
+DIR_TARGET=mecab-ko/mecab-ko
+SOURCE_BASE_COMMIT_ID=415f47d6119c4387a83e9f942e4ea2d47acf5bdc
+SOURCE_LAST_COMMIT_ID=master
+TARGET_BASE_COMMIT_ID=5a169db0e00ce973cada1353d93b76bc8655b346
+TARGET_LAST_COMMIT_ID=master
+
 
 function fn_init() {
     # set working directory
@@ -17,21 +22,56 @@ function fn_copy() {
     cp -rf ${SOURCE} ${TARGET}
 }
 
+function fn_checkout() {
+    local DIR=$1
+    local COMMIT_ID=$2
+
+    PREVIOUS_DIR=$(pwd)
+    cd ${DIR}
+
+    git fetch
+    git pull
+    git checkout ${COMMIT_ID}
+    git submodule update --init --recursive
+
+    cd -
+}
+
 function fn_patch() {
-    local TARGET=$1
+    local DIR=$1
     local PATCH=$2
     local LEVEL=$3
 
     PATCH=$(realpath ${PATCH})
-    cd ${TARGET}
-    patch -p${LEVEL} -i ${PATCH}
+    cd ${DIR}
+
+    patch -p${LEVEL} -i ${PATCH} --force
+
+    cd -
+}
+
+function fn_fix_version() {
+    local DIR=$1
+    WRONG_VERSION_FILE="swig/version.h"
+    CORRECT_VERSION_FILE="configure"
+
+    cd ${DIR}
+
+    WRONG_VERSION=$(grep "VERSION" ${WRONG_VERSION_FILE} | cut -d ' ' -f6 | sed 's/\"//g')
+    CORRECT_VERSION=$(grep "^\sVERSION" ${CORRECT_VERSION_FILE}| cut -d '=' -f2)
+
+    sed -i "s/${WRONG_VERSION//\//\\\/}/${CORRECT_VERSION//\//\\\/}/g" ${WRONG_VERSION_FILE}
+
     cd -
 }
 
 function fn_main() {
     fn_init
-    fn_copy ${DIR_SOURCE}/mecab ${DIR_TARGET}/
-    fn_patch ${DIR_TARGET}/mecab ${PATCH} 2
+    fn_copy ${DIR_SOURCE}/mecab ${DIR_ROOT}/
+    fn_patch ${DIR_ROOT}/mecab ./diff.cross.patch 2
+    fn_patch ${DIR_ROOT}/mecab ./diff.source.patch 1
+    fn_checkout ${DIR_TARGET} ${TARGET_LAST_COMMIT_ID}
+    fn_fix_version ${DIR_ROOT}/mecab
 }
 
 fn_main
